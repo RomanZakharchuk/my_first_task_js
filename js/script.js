4c7e62d2ba08675bb8da0fc4d92e8dad182c8041
Roman <henhok32@gmail.com>
2021-10-08 21:24:27 +0300

    add new question

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-"use sctrict";
+"use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = prompt('Скільки фільмів ви переглянули?', '');
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,15 +10,27 @@ const personalMovieDB = {
   privat: false
 }
 
+for (let i = 0; i < 2; i++) {
+  const a = prompt('Один з переглянутих фільмів?', '');
+  const b = prompt('Яка оцінка фільму?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log('DONE!');
+  } else {
+    console.log('ERROR!')
+    i--;
+  }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-
+if (personalMovieDB.count < 10) {
+  console.log('Переглянуто дуже мало фільмів!');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  console.log('Ви чудовий телеглядач!');
+} else if (personalMovieDB.count > 30) {
+  console.log('Ви кіноман!');
+} else {
+  console.log('Виникла помилка!')
+}
 
 console.log(personalMovieDB);
